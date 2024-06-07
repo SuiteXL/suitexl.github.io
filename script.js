feb34e503eaac9409dbcf1de40ebf695918d6fdc
Maks Grytsyuk <99030020+SuiteXL@users.noreply.github.com>
2024-06-07 00:14:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,30 +2,29 @@ let counter = 1.0;
 let counterInterval;
 
 function startCounter() {
-    counter = 1.0; // Reset counter
-    clearInterval(counterInterval); // Clear any existing interval
+    counter = 1.0;
+    clearInterval(counterInterval);
 
-    // Generate a random stop value between 1.0 and 3.0 each time the counter starts
     const stopValue = Math.random() * (3 - 1) + 1;
-    // console.log('Counter will stop at:', stopValue.toFixed(2));
+    // console.log('Counter will stop at:', stopValue.toFixed(2)); // USED FOR DEBUGGING AND TESTING
 
     const counterElement = document.getElementById('counter');
-    counterElement.style.color = 'green'; // Reset color to green
-    counterElement.textContent = counter.toFixed(2); // Reset counter text
-    counterElement.classList.remove('hidden'); // Make the counter visible
+    counterElement.style.color = 'green';
+    counterElement.textContent = counter.toFixed(2);
+    counterElement.classList.remove('hidden');
 
-    counterInterval = setInterval(() => updateCounter(stopValue), 100); // Update every 50 milliseconds
+    counterInterval = setInterval(() => updateCounter(stopValue), 100);
 }
 
 function updateCounter(stopValue) {
     const counterElement = document.getElementById('counter');
     counterElement.textContent = counter.toFixed(2); // Adjust toFixed() as needed
 
-    counter += 0.01; // Adjust increment to be smaller for more accuracy
+    counter += 0.01;
 
     if (counter >= stopValue) {
         clearInterval(counterInterval);
-        counter = stopValue; // Set counter to stopValue exactly
+        counter = stopValue;
         counterElement.style.color = 'red';
         console.log('Stopped at:', counter.toFixed(2));
     }
