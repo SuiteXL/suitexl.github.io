72a06c8ed92177ba660b9a45fa1f0084df4659d1
Maks Grytsyuk <99030020+SuiteXL@users.noreply.github.com>
2024-06-09 02:48:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,54 +9,56 @@ let countdownInterval;
 let crashInterval;
 
 function gameCountDown() {
-    let gameCooldown = 5; // Countdown
+  let gameCooldown = 5; // Countdown
 
-    document.getElementById("gameStartupCounter").innerHTML = gameCooldown.toFixed(1);
+  document.getElementById("gameStartupCounter").innerHTML =
+    gameCooldown.toFixed(1);
 
-    if (isPlaying === false) {
-        countdownInterval = setInterval(() => {
-            gameCooldown -= 0.1;
-            document.getElementById("gameStartupCounter").innerHTML = gameCooldown.toFixed(1);
-            if (gameCooldown <= 0) {
-                clearInterval(countdownInterval);
-                document.getElementById("gameStartupCounter").innerHTML = "GO!";
-                document.getElementById("counter").style.color = "#40a578";
-                isPlaying = true;
-                startGame();
-            }
-        }, 100);
-    }
+  if (isPlaying === false) {
+    countdownInterval = setInterval(() => {
+      gameCooldown -= 0.1;
+      document.getElementById("gameStartupCounter").innerHTML =
+        gameCooldown.toFixed(1);
+      if (gameCooldown <= 0) {
+        clearInterval(countdownInterval);
+        document.getElementById("gameStartupCounter").innerHTML = "GO!";
+        document.getElementById("counter").style.color = "#40a578";
+        isPlaying = true;
+        startGame();
+      }
+    }, 100);
+  }
 }
 
 function updateCrashCounter() {
-    const counterElement = document.getElementById("counter");
-    counterElement.textContent = crashCounter.toFixed(2);
+  const counterElement = document.getElementById("counter");
+  counterElement.textContent = crashCounter.toFixed(2);
 
-    if (crashCounter >= crashRange) {
-        clearInterval(crashInterval);
-        isCrashed = true;
-        isPlaying = false;
-        console.log("Crashed");
-        document.getElementById("counter").style.color = "red";
+  if (crashCounter >= crashRange) {
+    clearInterval(crashInterval);
+    isCrashed = true;
+    isPlaying = false;
+    console.log("Crashed");
+    document.getElementById("counter").style.color = "red";
 
-        // 3 Second Wait Before Restart
-        document.getElementById("gameStartupCounter").innerHTML = "Crashed!";
-        setTimeout(() => {
-            crashCounter = 1.0;
-            gameCountDown();
-        }, 3000);
-    } else {
-        crashCounter += crashCounterIncrement;
-    }
+    // 3 Second Wait Before Restart
+    document.getElementById("gameStartupCounter").innerHTML = "Crashed!";
+    setTimeout(() => {
+      crashCounter = 1.0;
+      gameCountDown();
+    }, 3000);
+  } else {
+    crashCounter += crashCounterIncrement;
+  }
 }
 
 function startGame() {
-    if (isPlaying) {
-        console.log("Game starting...");
-        crashRange = Math.random() * (2 - 1) + 1;
-        console.log(`New crash range: ${crashRange.toFixed(2)}`);
-        crashInterval = setInterval(updateCrashCounter, crashCounterInterval);
-    }
+  if (isPlaying) {
+    console.log("Game starting...");
+    crashRange = Math.random() * (2 - 1) + 1;
+    console.log(`New crash range: ${crashRange.toFixed(2)}`);
+    crashInterval = setInterval(updateCrashCounter, crashCounterInterval);
+  }
 }
 
 // Start the game for the first time
